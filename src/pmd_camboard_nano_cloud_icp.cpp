bf024210a7366e5c2e587498f337912f8bf81568
aleksandaratanasov <redbaronqueen@gmail.com>
2015-09-30 06:59:10 +0200

    Complete ICP but it still needs some work (especially the segmentation fault that occurs sometimes, see #4)

--- src/pmd_camboard_nano_cloud_icp.cpp
+++ src/pmd_camboard_nano_cloud_icp.cpp
@@ -81,11 +81,11 @@ private:
   u_int64_t fileIdx;
   std::ostringstream ss;
   bool toggleWritingToFile;
+  unsigned int fillStatus;
   unsigned int capacity;
   double maxCorrespondenceDistance;
   int maxIterations;
-  size_t fillStatus;
-  std::vector<pcl::PointCloud<pcl::PointNormal>::Ptr> clouds;
+  std::vector<pcl::PointCloud<pcl::PointNormal>::Ptr> *clouds;
 
   void pairAlign(pcl::PointCloud<pcl::PointNormal>::Ptr src, const pcl::PointCloud<pcl::PointNormal>::Ptr dst, pcl::PointCloud<pcl::PointNormal>::Ptr temp, Eigen::Matrix4f &final_transform) {
     // Instantiate our custom point representation (defined above) ...
@@ -94,9 +94,9 @@ private:
     float alpha[4] = {1., 1., 1., 1.};
     point_representation.setRescaleValues(alpha);
 
-
     // Align
     // TODO move IterativeClosestPointNonLinear object to a class member so that it won't be created all the time (settings are always the same so no need for that)
+    ROS_INFO("Setting up non linear ICP");
     pcl::IterativeClosestPointNonLinear<pcl::PointNormal, pcl::PointNormal> reg;
     reg.setTransformationEpsilon(1e-6);
     // Set the maximum distance between two correspondences in source and destination to 10cm
@@ -113,7 +113,7 @@ private:
     pcl::PointCloud<pcl::PointNormal>::Ptr reg_result = src;
     reg.setMaximumIterations(maxIterations); // TODO Move max iterations to launch file
 
-    for(int i = 0; i < 30; ++i) {
+    for(int i = 0; i < 30; ++i) { // See what this 30 is. Isn't his maxIterations?
       src = reg_result;
       // Estimate
       reg.setInputSource(src);
@@ -147,9 +147,10 @@ private:
     Eigen::Matrix4f globalTransform = Eigen::Matrix4f::Identity(), pairTransform;
 
     // Downsample (here we have < 20000 points per cloud so no need for that)
-    for(size_t cloud = 0; cloud < clouds.size(); cloud++) {
-      src = clouds.at(cloud - 1);
-      dst = clouds.at(cloud);
+    ROS_INFO("Running ICP for %lu clouds", clouds->size());
+    for(unsigned int cloud = 1; cloud < clouds->size(); cloud++) {
+      src = clouds->at(cloud - 1);
+      dst = clouds->at(cloud);
       pcl::PointCloud<pcl::PointNormal>::Ptr temp(new pcl::PointCloud<pcl::PointNormal>);
       pairAlign(src, dst, temp, pairTransform);
 
@@ -158,28 +159,33 @@ private:
 
       // Update global transform based on the transform of the current pair
       globalTransform = globalTransform * pairTransform;
+      ROS_INFO("Done %u out of %lu", cloud, clouds->size());
     }
 
     return result;
   }
 
 public:
-  CloudProcessingNodeICP(std::string topicIn, std::string topicOut)
-    : fileIdx(0), fillStatus(0)
+  CloudProcessingNodeICP(std::string topicIn, std::string topicOut, unsigned int _capacity)
+    : fileIdx(0), fillStatus(0), capacity(_capacity)
   {
     sub = nh.subscribe<sensor_msgs::PointCloud2>(topicIn, 5, &CloudProcessingNodeICP::subCallback, this);
     pub.advertise(nh, topicOut, 1);
-    clouds.reserve(capacity);
+    clouds = new std::vector<pcl::PointCloud<pcl::PointNormal>::Ptr>(capacity);
   }
 
   ~CloudProcessingNodeICP()
   {
     sub.shutdown();
+    delete clouds;
   }
 
   void setWritingToFile(bool _toggle) { toggleWritingToFile = _toggle; }
 
-  void setCapacity(unsigned int _capacity) { capacity = _capacity; }
+  void setCapacity(unsigned int _capacity) {
+    capacity = _capacity;
+    clouds->reserve(capacity);
+  }
 
   void setMaxCorrespondenceDist(double _maxCorrespondenceDistance) { maxCorrespondenceDistance = _maxCorrespondenceDistance; }
 
@@ -202,26 +208,24 @@ public:
 
     // TODO Add multithreading here - fill a container, pass it onto a thread (copy of it), repeat
     // Fill the vector with point clouds until the limit is reached
-    if(fillStatus != capacity) {
+    if(fillStatus < capacity) {
       ROS_INFO_STREAM("Current capacity: " << fillStatus << " out of " << capacity);
-      clouds.at(fillStatus) = p;  // Add received cloud to the sequence
+      clouds->at(fillStatus) = p;  // Add received cloud to the sequence
       fillStatus++;
     }
     else {
       // Run the vector of clouds through ICP and generate the final cloud
       // This is triggered once enough samples have been gathered
-      // Source: http://pointclouds.org/documentation/tutorials/pairwise_incremental_registration.php
-      // Source: http://pointclouds.org/documentation/tutorials/interactive_icp.php
+      // Source: http://pointclouds->org/documentation/tutorials/pairwise_incremental_registration.php
+      // Source: http://pointclouds->org/documentation/tutorials/interactive_icp.php
       pcl::PointCloud<pcl::PointNormal>::Ptr p_icp(runICP());
       ROS_INFO("Cleaning sample sequence");
-      //clouds.clear(); // Don't use clear since it reduces the size of the vector to 0, which 1)might lead to crash and 2)misses the point of allocating the memory for the container in advance
       fillStatus = 0;
 
       // Optional: write result to a binary compressed PCD file
       if(toggleWritingToFile)
       {
-        //std::string path = "/home/USER/catkin_ws/devel/lib/pmd_camboard_nano/";
-        std::string path = "~/catkin_ws/devel/lib/pmd_camboard_nano/";
+        std::string path = "/home/redbaron/catkin_ws/src/pmd_camboard_nano/samples/temp/";
         ss << path << "cloud_icp_" << fileIdx << ".pcd";
         pcl::io::savePCDFileBinaryCompressed(ss.str(), *p_icp);
         fileIdx++;
@@ -238,9 +242,9 @@ public:
 
 int main(int argc, char* argv[])
 {
-  ros::init (argc, argv, "cloud_template");
+  ros::init (argc, argv, "cloud_icp");
   ros::NodeHandle nh("~");
-  std::string topicIn = "points_sor";
+  std::string topicIn = "points_ne";  // Currently the implementation of the CloudProcessingNodeICP uses only clouds of type pcl::PointNormal so topicIn has to be of the same type
   std::string topicOut = "points_icp";
   bool toggleWriteToFile;
   int capacity;
@@ -257,13 +261,18 @@ int main(int argc, char* argv[])
   nh.param("maxCorrespondenceDist", maxCorrespondenceDistance, .1);
   nh.param("maxIterations", maxIterations, 2);
 
-  CloudProcessingNodeICP c(topicIn, topicOut);
-  ROS_INFO_STREAM("Writing to files " << (toggleWriteToFile ? "activated" : "deactivated"));
+  CloudProcessingNodeICP c(topicIn, topicOut, capacity);
+  ROS_INFO_STREAM("Writing to files: " << (toggleWriteToFile ? "enabled" : "disabled") << "\n"
+                  << "Capacity: " << capacity << "\n"
+                  << "Max. correspondence distance: " << maxCorrespondenceDistance << "\n"
+                  << "Max. iterations: " << maxIterations);
   c.setWritingToFile(toggleWriteToFile);
+  //c.setCapacity((unsigned int)capacity); // Can be used to adjust the capacity later on
+  c.setMaxCorrespondenceDist(maxCorrespondenceDistance);
+  c.setMaxIterations(maxIterations);
 
   while(nh.ok())
     ros::spin();
 
   return 0;
 }
-
